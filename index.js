78123c992662965f6312cbdda5665c8e154df5de
Johny Junior Duval <johny.duval@gmail.com>
2015-12-01 19:23:00 +0000

    Part 1: Getting Started!

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var app = express();
 
 var db = require('mysql');
 var connection = db.createConnection({
-    user: 'ziad_saab',
+    user: 'johnyduval',
     host: '127.0.0.1',
     database: 'addressbook'
 });
 
-var app = express();
+app.use(function(req, res, next){
+    req.accountId = 1;
+    next();
+});
+
 app.use(bodyParser.json());
 
-// app.get('...
+ app.get('/AddressBook/:accountId', function (req,res){
+     connection.query('select * from Account where id =' + req.accountId, function(err,result){
+        if (err){
+            console.log(err);
+        } else {
+            console.log(result);
+        }
+     });
+ });
+
+
+var server = app.listen(process.env.PORT, process.env.IP, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
